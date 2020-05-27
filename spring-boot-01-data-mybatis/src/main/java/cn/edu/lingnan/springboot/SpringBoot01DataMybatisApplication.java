3175d73536669c7dd798e397343f6eb222432e66
chenmancheng <59690424+chenmancheng@users.noreply.github.com>
2020-05-28 00:59:59 +0800

    excel to mysql

---
+++ spring-boot-01-data-mybatis/src/main/java/cn/edu/lingnan/springboot/SpringBoot01DataMybatisApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.lingnan.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot01DataMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot01DataMybatisApplication.class, args);
+    }
+
+}
